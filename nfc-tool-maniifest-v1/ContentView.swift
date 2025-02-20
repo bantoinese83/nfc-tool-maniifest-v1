8775de68d1798906f83236fe99f3d198044b7154
bantoinese83 <b.antoine.se@gmail.com>
2025-02-20 11:19:30 -0500

    Initial Commit

---
+++ nfc-tool-maniifest-v1/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  nfc-tool-maniifest-v1
+//
+//  Created by DOE PESCI on 2/20/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ nfc-tool-maniifest-v1/nfc_tool_maniifest_v1App.swift
@@ -0,0 +1,17 @@
+//
+//  nfc_tool_maniifest_v1App.swift
+//  nfc-tool-maniifest-v1
+//
+//  Created by DOE PESCI on 2/20/25.
+//
+
+import SwiftUI
+
+@main
+struct nfc_tool_maniifest_v1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
